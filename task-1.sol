e1d8333104cb0387522759a34fa49f4ba0eeedc2
Anas Aqeel <71166989+Anas-Aqeel@users.noreply.github.com>
2022-08-23 06:30:48 -0700

    Update task-1.sol

--- task-1.sol
+++ task-1.sol
@@ -1,11 +1,12 @@
 contract TaskOne{
-string public greet = "Hello Solidity";
-uint stateVar = 10;
-function returnStateVar() public view returns(uint){
-return stateVar;
-}
-function returnLocalVar() public pure returns(uint){
-uint localVar = 20;
-return localVar;
-}
+    string public greet = "Hello Solidity";
+    uint stateVar = 10;
+
+    function returnStateVar() public view returns(uint){
+        return stateVar;
+    }
+    function returnLocalVar() public pure returns(uint){
+        uint localVar = 20;
+        return localVar;
+    }
 }

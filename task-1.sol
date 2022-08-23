51d4e46a4eead5e09fb2c7e72e25d9a076404010
Anas Aqeel <71166989+Anas-Aqeel@users.noreply.github.com>
2022-08-23 16:33:10 +0500

    Add files via upload

---
+++ task-1.sol
@@ -0,0 +1,11 @@
+contract TaskOne{
+string public greet = "Hello Solidity";
+uint stateVar = 10;
+function returnStateVar() public view returns(uint){
+return stateVar;
+}
+function returnLocalVar() public pure returns(uint){
+uint localVar = 20;
+return localVar;
+}
+}

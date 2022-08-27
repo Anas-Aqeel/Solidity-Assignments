eaea2b418ea97843c486f29fb923204d8ee4ff72
Anas-Aqeel <anasaqeel.5555@gmail.com>
2022-08-27 01:46:52 -0700

    Task Completed

--- task-1.sol
+++ task-1.sol
@@ -10,3 +10,11 @@ contract TaskOne{
         return localVar;
     }
 }
+    // function nthTerm(uint n, uint a, uint b, uint c) public pure returns(uint result){
+    //     require(n > 3 , "n must be greater than 3");
+    //     uint[4] memory nums = [a,b,c, result];
+    //     for(uint i = 3; i < n; i++){
+    //         nums[3]= nums[i-1] + nums[i-2] + nums[i-3];
+    //     }
+    //     result = nums[nums.length-1];
+    // }

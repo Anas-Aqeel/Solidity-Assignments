c7a42573f22d91db2b7c8807ad61968d6c6f7a4b
Anas-Aqeel <anasaqeel.5555@gmail.com>
2022-09-03 00:03:02 -0700

    task 19

--- task-19.sol
+++ task-19.sol
@@ -2,16 +2,11 @@
 pragma solidity ^0.8.0;
 
 contract Task_19{
- function HFC(int[] memory array, uint length) public pure returns(int){
-            int secondMax;
-            int max;
-            for(uint i =0; i < length; i++){
-                if(array[i] > max){
-                    secondMax = max;
-                    max= array[i];
-                }
-                if(array[i] > secondMax && array[i] < max) secondMax = array[i];
-            }  
-            return secondMax;          
-        }
+  function hcf(uint num1,uint num2) public pure returns(uint){
+      if(num2 != 0){
+         return hcf(num2 , num1%num2);
+      }else{
+          return num1;
+      }
+  }
 }

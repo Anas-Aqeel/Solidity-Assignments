860d23cd81a7c54232bf029d6e09770dcf77ac15
Anas-Aqeel <anasaqeel.5555@gmail.com>
2022-09-10 07:02:33 -0700

    Voting Contract

--- VotingContract.sol
+++ VotingContract.sol
@@ -1,40 +1,57 @@
 /// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
- struct Vote{
+struct Vote{
       address person;
-      string candidate;
+      string party;
       bool isVoted;
+      uint power;
 }  
 
 contract Day1 {
-    mapping(string => uint) candidates;
+    mapping(string => uint) public parties;
+    mapping(address => bool) isBlackvoters;
+    mapping(address=>Vote) votes;
+    mapping(address=>Vote) blacklistVotes;
 
-    constructor(string[] memory _candidates){
-     for(uint i = 0;i<_candidates.length;i++){
-       candidates[_candidates[i]] = 1;
+    constructor(string[] memory _parties){
+     for(uint i = 0; i<_parties.length; i++){
+       parties[_parties[i]] = 1;
      }
     }
-    mapping(address=>Vote) votes;
 
-    function castVote(string memory _candidate) public {
+    function castVote(string memory _party) public {
 
-      require(candidates[_candidate] != 0, 'Invalid Candidate');
+      require(parties[_party] != 0, 'Invalid party');
+      require(!isBlackvoters[msg.sender], 'You are blacklisted');
       require(!votes[msg.sender].isVoted, 'Already voted');
-
-      votes[msg.sender] = Vote(msg.sender,'_candidate', true);
-      candidates[_candidate]+=1;
+      votes[msg.sender] = Vote(msg.sender,'_party', true, 1);
+      parties[_party]+=1;
     }
 
-    function status(string memory _candidate) public view returns(uint){
-      require(candidates[_candidate] != 0, 'Invalid Candidate');
-      
-      return candidates[_candidate]-1;
+    function status(string memory _party) public view returns(uint){
+      require(parties[_party] != 0, 'Invalid party');
+      return parties[_party]-1;
     }
 
     function checkMyvote()public view returns(Vote memory){
       return votes[msg.sender];
     }
 
+    function WhitelistVoter(address _voter) public{
+      require(!isBlackvoters[_voter], 'Already Whitelisted');
+      votes[msg.sender] = blacklistVotes[msg.sender];
+      isBlackvoters[_voter] = false;
+    }
+
+    function BlacklistVoter(address _voter) public{
+      require(isBlackvoters[_voter], 'Already Blacklisted');
+      Vote memory voter = votes[msg.sender];
+      parties[voter.party] -= voter.power;
+      votes[msg.sender] = Vote(address(0), '' , false ,0);
+      blacklistVotes[msg.sender] = voter;
+      isBlackvoters[_voter] = true;
+    }
+    
 }
 
